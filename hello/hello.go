36f506b9d6b1637cdd191dd9be9532aed09a9bd6
egostigma <30386767+egostigma@users.noreply.github.com>
2022-04-04 18:18:03 +0700

    init

---
+++ hello/hello.go
@@ -0,0 +1,20 @@
+package hello
+
+// User user type
+type User struct {
+	ID   int64
+	Name string
+	Addr *Address
+}
+
+// Address address type
+type Address struct {
+	City   string
+	ZIP    int
+	LatLng [2]float64
+}
+
+// Hello writes a welcome string
+func Hello() string {
+	return "Hello world"
+}
