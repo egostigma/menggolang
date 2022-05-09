7e39d0f9281f8eeec5945b3011d43533cafb686a
egostigma <mustopamahmud@icloud.com>
2022-05-09 16:43:55 +0700

    fix: Todo model type

--- graph/model/todo.go
+++ graph/model/todo.go
@@ -1,7 +1,7 @@
 package model
 
 type Todo struct {
-	ID   string `json:"id"`
+	ID   string `json:"id" bson:"_id"`
 	Text string `json:"text"`
 	Done bool   `json:"done"`
 	User *User  `json:"user"`
